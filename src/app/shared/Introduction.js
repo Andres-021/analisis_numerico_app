e9a9c74874c998c085efb87e8fdbd704f8097b30
juan <juanfelipe_1929@hotmail.com>
2022-05-20 14:51:20 -0500

    Se adicionaron los obgetivos del proyecto

--- src/app/shared/Introduction.js
+++ src/app/shared/Introduction.js
@@ -3,30 +3,60 @@ const Introduction = () => {
   return(
     <>
       <div>
-        <h4>PROYECTO FINAL ANALISIS NUMERICO</h4>
+      <font color="red"> <h4>PROYECTO FINAL ANALISIS NUMERICO</h4></font>
+        
       </div>
       <div>
         <p>
         En este proyecto se intenta demostrar los conocimientos impartido por
         nuestro profesor de analisis numerico.
         
-        Durante el desarrollo de esta asignatura, desarrollada en el transcurso
+        Durante el desarrollo de esta asignatura, realizada en el transcurso
         de el 2022-1 (Segundo Semestre de dosmil veintidos).
+
+        
         </p>
         <div>
-        <h4>ANALISIS NUMERICO</h4>
+        <font color="red"> <h4>OBJETIVOS DEL PROYECTO</h4></font>
+       
+      </div>
+      
+       <p> 
+        El objetivo de este proyecto es proporcionar una herramienta fácil de usa e interactiva, que nos permita realizar cálculos referentes al Analisis Numericos, 
+        de manera automatizada y rapída. 
+        </p>
+      
+        
+
+
+
+        <div>
+        <font color="red"> <h4>ANALISIS NUMERICO</h4></font>
+        
       </div>
         <p>
-          El análisis numérico o cálculo numérico es la rama de las matemáticas que encargada de diseñar algoritmos para,
-          a través de números y reglas matemáticas simples, simular procesos matemáticos más complejos aplicados a procesos
-          del mundo real.
-        </p>
+        El Calculo Numerico, o como tambien se le denomina, el Analisis numerico, es
+        la rama de las Matematicas que estudia los metodos numericos de resolucion de
+        problemas, es decir, los metodos que permiten obtener una solucion aproximada
+        (en ocasiones exacta) del problema considerado tras realizar un numero finito de
+        operaciones logicas y algebraicas elementales.</p>
+        <p>
+        Los problemas que trata el Analisis numerico se pueden clasificar en dos grandes
+        grupos, segun tengan naturaleza numerica (o finito dimensional) o naturaleza funcional (o infinito dimensional). Pertenecen al primer grupo los problemas relativos a
+        la resolucion de sistemas de ecuaciones lineales, calculo de valores y vectores propios,
+        y resolucion de ecuaciones y sistemas de ecuaciones no lineales. Son del segundo
+        tipo, por el contrario, los problemas de interpolacion y aproximacion de funciones, la
+        derivacion e integracion numericas, los problemas de valor inicial y de contorno para
+        ecuaciones diferenciales ordinarias, y los problemas de contorno para ecuaciones en
+        derivadas parciales.</p>
+        
         <div>
-        <iframe style={{
-          marginTop: '45px',
+ 
+        <img style={{
+          marginTop: '50px',
           width: "100%",
           height: "600px",
-        }} src="https://www.youtube.com/watch?v=1Gng8xl6I1w"></iframe>
+        }} src="https://definicion.xyz/wp-content/uploads/2020/11/analisis-numerico-700x467.jpg?ezimgfmt=ng%3Awebp%2Fngcb3%2Frs%3Adevice%2Frscb3-1"></img>
         </div>
       </div>
     </>
